00b84c5f42bea039a1c996d690da2f3dce868e92
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-13 12:25:27 -0400

    tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    if self._value.endswith('.'):
+      return True
+    else:
+      return False
+    
+  def is_question(self):
+    if self._value.endswith('?'):
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self._value.endswith('!'):
+      return True
+    else:
+      return False
+  
+  def count_sentences(self):
+    tem_value = self._value.replace('!', '.').replace('?', '.')
+    sentences = tem_value.split('.')
+
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    return len(sentences)
+  
